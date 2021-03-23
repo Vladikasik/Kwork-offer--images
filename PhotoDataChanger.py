0a88e0aa83ccaaad6859f46674da97f35156f020
Vladislav Ainshtein <vsakayafignavlad@gmail.com>
2021-03-23 08:31:48 +0000

    start

--- PhotoDataChanger.py
+++ PhotoDataChanger.py
@@ -1,7 +1,20 @@
 from exif import Image
+from log_all import Loger
 
-with open('my_test1.jpg', 'rb') as file:
-    my_image = Image(file)
+class ImageEditor:
 
-for i in my_image.list_all():
-    print(i)
+    def __init__(self, filename, query=None):
+        
+        self.log = Loger()
+
+        self.filenmae = filename
+        self.query = query
+    
+    def _edit_image(self):
+        
+        if not selfr.query:
+            self.log.no_arg(self.filename)
+            self._return_error_image
+    
+    def _return_error_image(self):
+        print('Here is error image returning') # TODO replace with error image with explonation
