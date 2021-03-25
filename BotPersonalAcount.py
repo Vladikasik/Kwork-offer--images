69c74e168afd29b16d5a69fc40cadedc5afb0578
Vlad <vsakayafignavlad@gmail.com>
2021-03-25 21:04:38 +0300

    Pep8

--- BotPersonalAcount.py
+++ BotPersonalAcount.py
@@ -1,23 +1,30 @@
 import telebot
 import config
+from telebot import types
+
 
 class Bot:
 
     def __init__(self):
 
         self.bot = telebot.TeleBot(config.token)
-    
+
+        self.main_message = None
+
+        self.main_menu_keyboard = types.InlineKeyboardMarkup
+
     def mainloop(self):
 
         @self.bot.message_handler(commands=['start', 'help', 'id'])
         def send_welcome(message):
-            print(message)
-            if message == "/start":
-                self.bot.send_message(message.from_user.id, "Здравствуйте, рады привествовать вас в боте!")
-            elif message == "/help":
-                self.bot.send_message(message.from_user.id, "Если есть вопросы/предложения, пешите @vladislav_ain")
-            elif message == '/id':
-                self.bot.send_message(message.from_user.id, f"Ваш id {str(message.from_user.id)}")    
+            if message.text == "/start":
+                self.bot.send_message(message.chat.id, "Здравствуйте, рады привествовать вас в боте!")
+                self.main_message = self.bot.send(message.chat.id, "Управляйте бот кнопками\nВсё интуитивно понятно\nЕсли возникли вопросы напишите /help")
+            elif message.text == "/help":
+                self.bot.send_message(message.chat.id, "Если есть вопросы/предложения, пешите @vladislav_ain")
+
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def buttons(call):
 
 
         self.bot.polling()
--- PhotoDataChanger.py
+++ PhotoDataChanger.py
@@ -1,25 +1,26 @@
 from exif import Image
 from log_all import Loger
 
+
 class ImageEditor:
 
     def __init__(self, filename, query_edit=None):
-        
+
         self.log = Loger()
         self.log.welcome()
 
         self.filename = filename
         self.query = query_edit
-    
+
     def _edit_image(self):
-        
+
         self.log.start_edit(self.filename)
 
         if not self.query:
             self.log.no_arg(self.filename)
             self._return_error(answer='NoQuery')
             return
-        
+
         try:
             image_data = self._image_data()
             self.log.loaded(self.filename)
@@ -27,14 +28,13 @@ class ImageEditor:
         except Exception as ex:
             self._return_error(answer=str(ex))
 
-    
     def _image_data(self):
-        
+
         with open(self.filename, 'rb') as image_file:
             image_data = Image(image_file)
-        
+
         return image_data
-    
+
     def _test_print_values(self, image_data):
         exit_str = ''
         for i in image_data.list_all():
@@ -47,6 +47,6 @@ class ImageEditor:
                     exit_str += str(i) + ':' + 'cannot get it' + '\n'
         with open('all_data.txt', 'w') as f:
             f.write(exit_str)
-    
+
     def _return_error(self, answer):
-        print(f'Here is error {answer}') # TODO replace with error image with explonation
+        print(f'Here is error {answer}')  # TODO replace with error image with explonation
