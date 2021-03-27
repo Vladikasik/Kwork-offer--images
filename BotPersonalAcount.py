3e76aa02cc099b18ce2f95a811fab598ef2be630
Vlad <vsakayafignavlad@gmail.com>
2021-03-27 13:35:21 +0300

    Bot added + comments to PhotoDataChanger

--- BotPersonalAcount.py
+++ BotPersonalAcount.py
@@ -9,9 +9,18 @@ class Bot:
 
         self.bot = telebot.TeleBot(config.token)
 
-        self.main_message = None
+        self.main_message = {}
+        self.settings = {}
 
-        self.main_menu_keyboard = types.InlineKeyboardMarkup
+        self.main_menu_keyboard = types.InlineKeyboardMarkup(row_width=1)
+        self.main_menu_keyboard.add(types.InlineKeyboardButton(text='Изменить данные фотографии',
+                                                               callback_data='change_photo'),
+                                    types.InlineKeyboardButton(text='Баланс',
+                                                               callback_data='balance'))
+
+        self.back_button_keyboard = types.InlineKeyboardMarkup(row_width=1)
+        self.back_button_keyboard.add(types.InlineKeyboardButton(text='⬅Вернуться в главное меню',
+                                                                 callback_data='back_to_main'))
 
     def mainloop(self):
 
@@ -19,15 +28,66 @@ class Bot:
         def send_welcome(message):
             if message.text == "/start":
                 self.bot.send_message(message.chat.id, "Здравствуйте, рады привествовать вас в боте!")
-                self.main_message = self.bot.send(message.chat.id, "Управляйте бот кнопками\nВсё интуитивно понятно\nЕсли возникли вопросы напишите /help")
+                self.bot.send_message(message.chat.id,
+                                      "Управляйте бот кнопками\n"
+                                      "Всё интуитивно понятно\n"
+                                      "Если возникли вопросы напишите /help",
+                                      reply_markup=self.main_menu_keyboard)
             elif message.text == "/help":
                 self.bot.send_message(message.chat.id, "Если есть вопросы/предложения, пешите @vladislav_ain")
 
         @self.bot.callback_query_handler(func=lambda call: True)
         def buttons(call):
 
+            # juat because it is needed to be here idk
+            def _photo_edit(message):
+                # getting settings
+                data = self._get_query(message.text)
+                self.settings[message.from_user.id] = data
+                self.bot.send_message(call.message.chat.id, "Теперь пришлите фотографию, которую вы хотите изменить")
+
+            if call.data == 'change_photo':
+                self.bot.send_message(call.message.chat.id,
+                                      "Ваш баланс состовляет {} коинов. Вы можете сделать {} запросов.\n")
+                self.bot.send_message(call.message.chat.id,
+                                      "Следующим сообщением напиши настройки для изменения данных\n\n"
+                                      "1) Время - используеться только дни часы минуты\n"
+                                      "Пример - ('1 мин', '1 час 1 секунда', '23 дня 5 часов 7 секунд'\n"
+                                      "в пределее указанного значения время в метаданных фотографии"
+                                      "будет изменено время(+- 1 минута, +- 1 час 1 минута)\n\n"
+                                      "2) Расположение - используются только метры (целое кол-во)\n"
+                                      "Пример - ('1 метр', '5 метров', '23 метра')"
+                                      "в пределее указанного значения время в метаданных фотографии"
+                                      "будет изменены gps координаты (+- 1 метр, +- 5 метров)\n\n"
+                                      )
+                self.bot.send_message(call.message.chat.id,
+                                      "Как может выглядеть ваш запрос")
+                self.bot.send_message(call.message.chat.id,
+                                      "1 дня 0 часов 3 минуты\n"
+                                      "20 метров")
+                a = self.bot.send_message(call.message.chat.id,
+                                          "Если я не пойму я сообщу, в конце, мы всё равно сверимся, всё ли верно",
+                                          reply_markup=self.back_button_keyboard
+                                          )
+                self.bot.register_next_step_handler(a, _photo_edit)
+
+            elif call.data == 'back_to_main':
+                self.bot.send_message(call.message.chat.id,
+                                      "Управляйте бот кнопками\n"
+                                      "Всё интуитивно понятно\n"
+                                      "Если возникли вопросы напишите /help",
+                                      reply_markup=self.main_menu_keyboard)
+
+            elif call.data == 'balance':
+                self.bot.send_message(call.message.chat.id,
+                                      "оплата бла бла бла",
+                                      reply_markup=self.back_button_keyboard)
 
         self.bot.polling()
+
+    def _get_query(self, text):
+        return {'time': 3,
+                'gps': 8}
 
 
 if __name__ == '__main__':
--- PhotoDataChanger.py
+++ PhotoDataChanger.py
@@ -6,45 +6,52 @@ class ImageEditor:
 
     def __init__(self, filename, query_edit=None):
 
-        self.log = Loger()
-        self.log.welcome()
+        self.log = Loger()  # creating logger object (this is my custom logging
+        self.log.welcome()  # logging about program start
 
         self.filename = filename
         self.query = query_edit
 
+        self.edit_time = None
+        self.edit_gps = None
+
+    # first and main function
     def _edit_image(self):
 
-        self.log.start_edit(self.filename)
+        self.log.start_edit(self.filename)  # logging about edit start
 
-        if not self.query:
+        if not self.query:  # if class were created without query what to edit
             self.log.no_arg(self.filename)
             self._return_error(answer='NoQuery')
-            return
+            return  # exiting func after logging exception
 
         try:
             image_data = self._image_data()
             self.log.loaded(self.filename)
             self._test_print_values(image_data)
-        except Exception as ex:
+        except Exception as ex:  # if something wrong with loading image
             self._return_error(answer=str(ex))
 
+    # load image + get metadate
     def _image_data(self):
 
         with open(self.filename, 'rb') as image_file:
-            image_data = Image(image_file)
+            image_data = Image(image_file)  # getting data via library
 
         return image_data
 
+    # just print value_name:value_value
     def _test_print_values(self, image_data):
         exit_str = ''
         for i in image_data.list_all():
-            try:
+            try:  # it a[1] value
                 exit_str += str(i) + ':' + str(image_data[i]) + '\n'
             except:
-                try:
+                try:  # it also can be a.1 value
                     exit_str += str(i) + ':' + str(image_data.i) + '\n'
-                except:
+                except:  # idk what to do then
                     exit_str += str(i) + ':' + 'cannot get it' + '\n'
+        #  write all the data to txt file to watch it then
         with open('all_data.txt', 'w') as f:
             f.write(exit_str)
 
