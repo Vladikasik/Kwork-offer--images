64a4705816dabc7aee61f1fd952457831b2e5d56
Vladislav Ainshtein <vsakayafignavlad@gmail.com>
2021-03-31 10:57:13 +0000

    datetime edit

--- TimeEditor.py
+++ TimeEditor.py
@@ -4,4 +4,9 @@ class TimeEditor:
 
     def __init__(self, str_time):
         
-        self.start_time = datetime.strptime(str_time, '%b %d %Y %I:%M%p')
+        self.start_time = datetime.strptime(str_time, '%Y:%m:%d %H:%M:%S')
+        print(self.start_time)
+
+if __name__=='__main__':
+    editor = TimeEditor('2021:03:23 03:39:50')
+    
